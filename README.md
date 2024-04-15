07ecf2f1f57fe8f3c3df03641bb1ea7405b696ae
Rafael M. da Silva <123322146+rafaelmdasilva@users.noreply.github.com>
2024-04-15 15:34:30 -0300

    Add to-do items

--- README.md
+++ README.md
@@ -4,21 +4,22 @@ A simple clock built with HTML, CSS and JavaScript.
 
 ### Features
 
-- Displays the current system time on a 12h format, followed by a greet.
+- Displays the system time on a 12h format, followed by a greet.
 - Changes the page color, greet and image accordingly.
 
 ### To-Do
 
-- Display minutes and seconds.
-- Have the clock run in real time.
+- ~~Display minutes and seconds.~~
+- ~~Have the clock run in real time.~~
 - Have a 24h/12h toggle.
 - Responsive design.
+- Further code optimization
 
 ### Live Demo
 
 [![Github Pages](https://img.shields.io/badge/github%20pages-121013?style=for-the-badge&logo=github&logoColor=white)](https://rafaelmdasilva.github.io/current-time/)
 
-![Page Preview](./images/preview.png)
+[![Page Preview](./images/preview.png)](https://rafaelmdasilva.github.io/current-time/)
 
 ### Contributions
 
--- script.js
+++ script.js
@@ -1,58 +1,66 @@
-const currentTime = new Date()
-var hours = currentTime.getHours()
+'use strict';
+
 const greet = document.querySelector('#greeting')
 const clock = document.querySelector('#clock')
-
-let period = (hours >= 12 ? 'PM' : 'AM')
-var hours = ((hours + 11) % 12 + 1)
-let time = (`${hours} ${period}`)
-
-clock.innerHTML = (`Current time: ${time}`)
-
 const container = document.querySelector('.container')
 const image = document.querySelector('.sub-container img')
 
-function morning() {
-    greet.innerHTML = ('Good morning')
-    container.style.background = ('linear-gradient(225deg, rgba(135,207,235,1) 0%, rgba(248,219,123,1) 100%)')
-    image.src = ('./images/Layer1.png')
-}
-function afternoon() {
-    greet.innerHTML = ('Good afternoon')
-    container.style.background = ('linear-gradient(180deg, rgba(135,207,235,1) 0%, rgba(255,255,255,1) 100%)')
-    image.src = ('./images/Layer2.png')
-}
-function evening() {
-    greet.innerHTML = ('Good evening')
-    container.style.background = ('linear-gradient(125deg, rgba(135,207,235,1) 0%, rgba(255,68,0,1) 100%)')
-    image.src = ('./images/Layer3.png')
-}
-function night() {
-    greet.innerHTML = ('Good night')
-    container.style.background = ('linear-gradient(180deg, rgba(35, 35, 71,1) 0%, rgba(0,0,0,1) 100%)')
-    image.src = ('./images/Layer4.png')
-}
+setInterval(() => {
 
-if (period == 'AM') {
-    if (hours >= 1) {
-        night()
-    } if (hours >= 5) {
-        morning()
-    }
-}
-if (period == 'PM') {
-    if (hours >= 1) {
-        afternoon()
-    } if (hours >= 5) {
-        evening()
-    } if (hours >= 8) {
-        night()
-    }
-}
-if (hours == 12) {
+    const currentTime = new Date()
+    const currentHours = currentTime.getHours()
+    const currentMinutes = currentTime.getMinutes()
+    const currentSeconds = currentTime.getSeconds()
+
+    const period = (currentHours >= 12 ? 'PM' : 'AM')
+    const hours = ((currentHours + 11) % 12 + 1)
+    const time = (`${hours}:${currentMinutes}:${currentSeconds} ${period}`)
+
+    clock.innerHTML = (`Current time: ${time}`)
+
     if (period == 'AM') {
-        night()
-    } else {
-        afternoon()
+        if (hours >= 1) {
+            night()
+        } if (hours >= 5) {
+            morning()
+        }
     }
-}
+    if (period == 'PM') {
+        if (hours >= 1) {
+            afternoon()
+        } if (hours >= 5) {
+            evening()
+        } if (hours >= 8) {
+            night()
+        }
+    }
+    if (hours == 12) {
+        if (period == 'AM') {
+            night()
+        } else {
+            afternoon()
+        }
+    }
+
+    function morning() {
+        greet.innerHTML = ('Good morning')
+        container.style.background = ('linear-gradient(225deg, rgba(135,207,235,1) 0%, rgba(248,219,123,1) 100%)')
+        image.src = ('./images/Layer1.png')
+    }
+    function afternoon() {
+        greet.innerHTML = ('Good afternoon')
+        container.style.background = ('linear-gradient(180deg, rgba(135,207,235,1) 0%, rgba(255,255,255,1) 100%)')
+        image.src = ('./images/Layer2.png')
+    }
+    function evening() {
+        greet.innerHTML = ('Good evening')
+        container.style.background = ('linear-gradient(125deg, rgba(135,207,235,1) 0%, rgba(255,68,0,1) 100%)')
+        image.src = ('./images/Layer3.png')
+    }
+    function night() {
+        greet.innerHTML = ('Good night')
+        container.style.background = ('linear-gradient(180deg, rgba(35, 35, 71,1) 0%, rgba(0,0,0,1) 100%)')
+        image.src = ('./images/Layer4.png')
+    }
+
+}, 1000)
